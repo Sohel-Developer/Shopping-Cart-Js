2cf43a32baa8bfdcb4fd295329b0fa0121d162c2
Sohel-Developer <sohel.webdevoloper@gmail.com>
2024-01-06 12:41:25 +0600

    Cart Item Phone Value and price Updateble

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 console.log("Cannected");
-let phoneValue = document.getElementById("phone_value");
 
+document.getElementById("phone_minus_btn").addEventListener("click", function () {
+    const phoneValueField = document.getElementById("phone_value")
+    const phoneValueString = phoneValueField.value;
+    const phoneValueNumber = parseInt(phoneValueString)
+
+    if (phoneValueNumber > 0) {
+        const newNumber = phoneValueNumber - 1;
+        phoneValueField.value = newNumber
+
+        const phoneValue = document.getElementById("phone_price")
+        const newPrice = newNumber * 1219
+        phoneValue.innerText = newPrice
+    } else {
+        alert("Wrong Interactivity")
+    }
+
+
+})
+
+
+
+document.getElementById("phone_plus_btn").addEventListener("click", function () {
+    const phoneValueField = document.getElementById("phone_value")
+    const phoneValueString = phoneValueField.value;
+    const phoneValueNumber = parseInt(phoneValueString)
+
+    const newNumber = phoneValueNumber + 1;
+    phoneValueField.value = newNumber
+
+
+    const phoneValue = document.getElementById("phone_price")
+    const newPrice = newNumber * 1219
+    phoneValue.innerText = newPrice
+})
