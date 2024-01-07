47785202c20150ca4056effe6637697f7ed5b631
Sohel-Developer <sohel.webdevoloper@gmail.com>
2024-01-07 14:28:01 +0600

    Phone Price Updated and clean code use regular function.

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-console.log("Cannected");
-
-document.getElementById("phone_minus_btn").addEventListener("click", function () {
-    const phoneValueField = document.getElementById("phone_value")
+function productPrice (isIncress,quantityId){
+    const phoneValueField = document.getElementById(quantityId)
     const phoneValueString = phoneValueField.value;
     const phoneValueNumber = parseInt(phoneValueString)
-
-    if (phoneValueNumber > 0) {
-        const newNumber = phoneValueNumber - 1;
-        phoneValueField.value = newNumber
-
-        const phoneValue = document.getElementById("phone_price")
-        const newPrice = newNumber * 1219
-        phoneValue.innerText = newPrice
-    } else {
-        alert("Wrong Interactivity")
+    let  newNumber =1 ;
+    if(isIncress){
+        newNumber= phoneValueNumber + 1;
+    }else{
+        if(phoneValueNumber > 0){
+        newNumber= phoneValueNumber - 1;
+        }else{
+            alert("Wrong Decision")
+            return phoneValueNumber
+        }   
+    }
+        phoneValueField.value=newNumber
+    return newNumber;     
     }
 
+function priceMultiply (newNumber,productPrice){
+    const phoneValue = document.getElementById("phone_price")
+    const newPrice = newNumber * productPrice;
+    phoneValue.innerText = newPrice
+}
 
+
+// Phone Price Functionality
+
+document.getElementById("phone_minus_btn").addEventListener("click", function () {
+    const newNumber = productPrice(false,"phone_value")
+    // Phone Price Is $1219
+    priceMultiply(newNumber, 1219)
 })
-
 
 
 document.getElementById("phone_plus_btn").addEventListener("click", function () {
-    const phoneValueField = document.getElementById("phone_value")
-    const phoneValueString = phoneValueField.value;
-    const phoneValueNumber = parseInt(phoneValueString)
+    const newNumber = productPrice(true,"phone_value")
+    // Phone Price Is $1219
+    priceMultiply(newNumber,1219)
 
-    const newNumber = phoneValueNumber + 1;
-    phoneValueField.value = newNumber
-
-
-    const phoneValue = document.getElementById("phone_price")
-    const newPrice = newNumber * 1219
-    phoneValue.innerText = newPrice
 })
+
+
